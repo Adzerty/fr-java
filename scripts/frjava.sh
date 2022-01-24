9999e5f7ce19d6f28091edbe9956aff46f4a9137
Adzerty <adzerty76@outlook.fr>
2022-01-24 01:13:22 +0100

    Fix : changement du script

--- scripts/frjava.sh
+++ scripts/frjava.sh
@@ -2,7 +2,6 @@
 function frjavac() {
     java frjava.Parser $1;
     name=$(echo "$1" | cut -f 1 -d '.');
-    echo $name;
-    javac $name.java
-    rm -f $name.java
+    javac $name.java;
+    rm -f $name.java;
 }   
